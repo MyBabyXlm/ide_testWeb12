77a83b6f65059f20ca32e06d9f3cf0e91f5d08e0
刘永鹏 <188822368@qq.com>
2018-12-22 22:25:17 +0800

    提交测试2

--- src/it/test.java
+++ src/it/test.java
@@ -3,5 +3,8 @@ package it;
 public class test {
     public static void main(String[] args) {
         System.out.println("ni daye");
+        System.out.println("ni daye2");
+        System.out.println("ni daye3");
+        System.out.println("ni daye4");
     }
 }
